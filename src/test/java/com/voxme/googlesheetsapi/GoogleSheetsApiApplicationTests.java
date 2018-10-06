4c7f918a6b6fb15adcf35110525d03f3014a7d98
Aleksandr Streltsov <jness.pro@gmail.com>
2018-10-06 22:28:33 +0300

    ...

--- src/test/java/com/voxme/googlesheetsapi/GoogleSheetsApiApplicationTests.java
+++ src/test/java/com/voxme/googlesheetsapi/GoogleSheetsApiApplicationTests.java
@@ -1,16 +1,24 @@
 package com.voxme.googlesheetsapi;
 
+import com.voxme.googlesheetsapi.service.GoogleApiGateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GoogleSheetsApiApplicationTests {
 
-    @Test
-    public void test() {
+    @Autowired
+    private GoogleApiGateway gateway;
 
+    @Test
+    public void test() throws GeneralSecurityException, IOException {
+        gateway.copyTemplate();
     }
 }
