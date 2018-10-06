dd6d82d9cfffe74f7b6e698acb92bdf83dbaa775
Aleksandr Streltsov <jness.pro@gmail.com>
2018-10-06 22:22:43 +0300

    ...

--- src/main/java/com/voxme/googlesheetsapi/service/GoogleApiGateway.java
+++ src/main/java/com/voxme/googlesheetsapi/service/GoogleApiGateway.java
@@ -1,5 +1,8 @@
 package com.voxme.googlesheetsapi.service;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public interface GoogleApiGateway {
-    void copyTemplate();
+    void copyTemplate() throws GeneralSecurityException, IOException;
 }
